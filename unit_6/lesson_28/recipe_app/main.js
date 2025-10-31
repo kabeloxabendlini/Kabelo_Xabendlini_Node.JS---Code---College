3c74acb4ed3756cb42e1aa7456db8fa1b644a65b
kabelo385 <kabeloxabendlini385@gmail.com>
2025-10-31 19:54:37 +0200

    editing commit

---
+++ unit_6/lesson_28/recipe_app/main.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+// const router = express.Router();
+const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
+const connectFlash = require("connect-flash");
+const passport = require("passport");
+const homeController = require("./controllers/homeController");
+const subscribersController = require("./controllers/subscribersController");
+const User = require("./models/user");
+const router = require("./routes/index");
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://0.0.0.0:27017/recipe_db", {
+  useNewUrlParser: true,
+});
+mongoose.set("useCreateIndex", true);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(layouts);
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser("secret_passcode"));
+app.use(
+  expressSession({
+    secret: "secret_passcode",
+    cookie: {
+      maxAge: 4000000,
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(connectFlash());
+
+//-----------------------------------------------------------------
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+app.use(homeController.logRequestPaths);
+
+// HOME ROUTES REMOVED
+
+// USER ROUTES REMOVED
+
+// SUBSCRIBER ROUTES REMOVED
+app.post("/subscribe", subscribersController.saveSubscriber);
+
+// COURSE ROUTES REMOVED
+
+// ERROR ROUTES REMOVED
+
+app.use("/", router);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
---
+++ unit_6/lesson_28/recipe_app/routes/homeRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const homeController = require("../controllers/homeController");
+
+
+router.get("/", homeController.index);
+router.get("/contact", homeController.getSubscriptionPage);
+
+module.exports = router;
